5bfdb65fc775ed3aa7573df9814732e0ac22e37f
PSARTORI <PSARTORI@PSARTORI-7420.al.oracle.com>
2022-04-27 19:41:37 -0300

    initial commit

---
+++ demo.tf
@@ -0,0 +1,22 @@
+variable "compartment_ocid" {}
+variable "region" {}
+variable "bucket_name" {}
+
+provider "oci" {
+    region = var.region
+}
+
+data "oci_objectstorage_namespace" "namespace" {
+    compartment_id = var.compartment_ocid
+}
+
+resource "oci_objectstorage_bucket" "create_bucket" {
+    compartment_id = var.compartment_ocid
+    name = var.bucket_name
+    namespace = data.oci_objectstorage_namespace.namespace.namespace
+    access_type = "NoPublicAccess"
+}
+
+output "new_bucket" {
+    value = oci_objectstorage_bucket.create_bucket
+}
